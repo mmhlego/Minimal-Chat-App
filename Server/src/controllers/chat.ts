76ad63d20f7346fe1547fdc91b37dd1c1a78ee90
Mahdi <test@gmail.com>
2023-06-19 03:41:53 +0330

    finish chat endpoints

--- Server/src/controllers/chat.ts
+++ Server/src/controllers/chat.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import { CustomReq, ResError } from "../app";
 import Chat, { IChat } from "../models/chat";
 import User from "../models/user";
+import Message from "../models/message";
 
 export const createChat: RequestHandler = (req, res, next) => {
 	const chatName = req.body.name;
@@ -92,6 +93,98 @@ export const getChatMembers: RequestHandler = (req, res, next) => {
 					});
 					return accumulator;
 				}, []),
+			});
+		})
+		.catch((err) => {
+			next(err);
+		});
+};
+
+export const addChatMember: RequestHandler = (req, res, next) => {
+	const chatId = req.params.chatId;
+	const usernames = req.body.members;
+	User.find({ username: { $in: [...usernames] } })
+		.then((users) => {
+			Chat.findOne({ _id: chatId }).then((chat) => {
+				if (!chat) throw new Error("chat id is not valid!");
+
+				users.forEach((user) => {
+					user.chatIds = [...user.chatIds, chatId];
+					chat.memberIds = [...chat.memberIds, user._id];
+					chat.save();
+					user.save();
+				});
+			});
+
+			res.status(200).json({
+				status: "success",
+				data: {},
+			});
+		})
+		.catch((err) => {
+			next(err);
+		});
+};
+
+export const sendMessage: RequestHandler = (req, res, next) => {
+	const chatId = req.params.chatId;
+	const replyTo = req.body.replyTo;
+	const body = req.body.body;
+	const message = new Message({
+		chatId: chatId,
+		senderId: (req as CustomReq).userId,
+		replyTo: replyTo,
+		body: body,
+		sendDate: new Date(),
+	});
+	message.save();
+
+	res.status(200).json({
+		status: "success",
+		data: {},
+	});
+};
+
+export const deleteMessage: RequestHandler = (req, res, next) => {
+	const messageId = req.params.messageId;
+	Message.deleteOne({ _id: messageId }).catch((err) => {
+		next(err);
+	});
+
+	res.status(200).json({
+		status: "success",
+		data: {},
+	});
+};
+export const getMessage: RequestHandler = (req, res, next) => {
+	const messageId = req.params.messageId;
+	Message.findOne({ _id: messageId }, { _id: 0, __v: 0 })
+		.then((message) => {
+			if (!message) throw new Error("messageId is invalid!");
+			res.status(200).json({
+				status: "success",
+				data: message,
+			});
+		})
+		.catch((err) => {
+			next(err);
+		});
+};
+
+export const getChatHistory: RequestHandler = (req, res, next) => {
+	const chatId = req.params.chatId;
+	const date = req.body.date;
+	const count = req.body.count;
+	Message.find(
+		{ $and: [{ chatId }, { sendDate: { $lt: date } }] },
+		{ _id: 0, __v: 0 },
+		{ sort: { sendDate: 0 }, limit: count }
+	)
+		.then((messages) => {
+			if (!messages) throw new Error("messageId is invalid!");
+			res.status(200).json({
+				status: "success",
+				data: messages,
 			});
 		})
 		.catch((err) => {
--- Server/src/routes/chat.ts
+++ Server/src/routes/chat.ts
@@ -1,11 +1,15 @@
 import { isAuth, login, signup } from "../controllers/auth";
 import {
+	addChatMember,
 	createChat,
+	deleteMessage,
 	getChat,
+	getChatHistory,
 	getChatList,
 	getChatMembers,
+	getMessage,
+	sendMessage,
 } from "../controllers/chat";
-import { getProfile, updateProfile } from "../controllers/profile";
 
 const express = require("express");
 
@@ -18,5 +22,15 @@ router.get("/chats", isAuth, getChatList);
 router.get("/chats/:chatId", isAuth, getChat);
 
 router.get("/chats/:chatId/members", isAuth, getChatMembers);
+
+router.post("/chats/:chatId/members", isAuth, addChatMember);
+
+router.post("/chats/:chatId/messages", isAuth, sendMessage);
+
+router.delete("/chats/:chatId/messages/:messageId", isAuth, deleteMessage);
+
+router.get("/chats/:chatId/messages/:messageId", isAuth, getMessage);
+
+router.post("/chats/:chatId/history", isAuth, getChatHistory);
 
 export default router;
