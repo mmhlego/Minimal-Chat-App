e492317115d2ce25ba1806f3737a95d54c9dc4ee
Mahdi <test@gmail.com>
2023-06-21 01:57:41 +0330

    socket io

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -5,6 +5,9 @@ import profileRouter from "./routes/profile";
 import chatRouter from "./routes/chat";
 import bodyParser from "body-parser";
 import { Server } from "socket.io";
+import Chat from "./models/chat";
+import Message from "./models/message";
+import jwt from "jsonwebtoken";
 
 export interface ResError extends Error {
 	status?: number;
@@ -61,7 +64,19 @@ mongoose
 			console.log("New connection created");
 			socket.emit("message_from_server", `You joined to the socket!`);
 
-			// const token = socket.handshake.auth.token;
+			const token = socket.handshake.auth.token;
+
+			const tok = token.split(" ")[1];
+			let decodedToken: any;
+			try {
+				decodedToken = jwt.verify(tok, "somesupersecretsecret");
+			} catch (err: any) {
+				socket.disconnect();
+			}
+			if (!decodedToken) {
+				socket.disconnect();
+			}
+
 			// console.log('Auth token', token);
 
 			// try {
@@ -96,10 +111,29 @@ mongoose
 			 * New code
 			 */
 
-			socket.on("send-message", (body: string, replyId?: string) => {
-				console.log("new message", { body, replyId });
-				socketIo.to(room).emit("receive-message", body, replyId);
-			});
+			socket.on(
+				"send-message",
+				(body: string, chatId: string, replyId?: string) => {
+					const message = new Message({
+						chatId: chatId,
+						senderId: (decodedToken as { userId: string }).userId,
+						replyTo: replyId,
+						body: body,
+						sendDate: new Date(),
+					});
+					message.save();
+					Chat.findOne({ _id: chatId })
+						.then((chat) => {
+							if (!chat) throw new Error("chatId is invalid!");
+							chat.lastMessage = body;
+							chat.lastSendDate = new Date();
+							chat.save();
+							socketIo.to(room).emit("receive-message", message);
+						})
+						.catch((err) => {});
+					console.log("new message", { body, replyId });
+				}
+			);
 		});
 	})
 	.catch((err) => console.log(err));
